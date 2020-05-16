dbcf59b4bdde9fe6f010037461905de0e8234b09
tariksitki <tariksitki0119@gmail.com>
2020-05-16 13:10:17 +0200

    jawascript exercise 1-9

--- main.js
+++ main.js
@@ -140,8 +140,164 @@ var today = new Date();
 
 
 
-    // Exercise 6  
+    // Exercise 6   Find Leap Year (Artik yil) 
 
+    // Formül: 1. Adim  Eger Yil 4 ile bölünüyorsa 2. adima git yani 100 ile böl. Eger 4 ile bölünmüyorsa artik yil degildir.
+    
+    //         2. Adim  Eger 4 e bölünüp 100 e bölünmüyorsa artik yildir. 4 e ve 100 e bölünüyorsa 3. adima git yani 400 e böl.
+
+    //         3. Adim  Eger 4 e 100 e ve 400 e  bölünüyorsa artik yildir. 4 e ve 100 e bölünüp 400 e bölünmezse artik yil degildir. 
+
+    function leapyear(Year) 
+    {
+    return (Year % 100 === 0) ? (Year % 400 === 0) : ( Year % 4 === 0) ;    
+    }
+
+    console.log(leapyear(2016));
+
+    console.log(leapyear(2000));
+
+    console.log(leapyear(1700));
+
+    console.log(leapyear(1800));
+
+    console.log(leapyear(100));
+
+    console.log(leapyear(104));
+
+
+
+
+    //  Exercise 7  A Program to find 1st Januar is being a Sunday between 2014 and 2050
+
+    // Notiz:  Mantigini anladim ama mesela 1st januar degilde 1st july Sunday oluyor mu diye sordugumuz da nasil yapacagiz soralim. 
+
+    // Notiz:  Bir de getDay===0 degil de 1 yaptigimiz da ne olacak
+
+
+
+
+    console.log("---------------------");
+
+    for ( var year = 2014 ; year <= 2050; year++)
+
+    { 
+        var d = new Date(year, 0 , 1 );
+
+        if ( d.getDay() === 0)
+
+            console.log("1st Januar is being a Sunday" + "  " + year );
+            
+    }
+
+    console.log("---------------------");
+
+
+
+
+
+    // Exercise 8  Get a random integer from 1 to 10 inclusive
+
+    // ilk yazdigimiz const num formulü ile sayfa her yenilendiginde console da 1 ile 10 arasinda otomatik bir sayi atiyoruz. 
+
+    // 2. const ile sayfa acildiginda ve yenilendigin de prompt aciyoruz. Eger musteri prompt a console da otomatik belirlenen degeri girdi ise console da Matched yaziyor. Degilse not matched.
+
+
+
+    const num = Math.ceil(Math.random() * 10) ;
+
+    console.log(num);
+
+        const gnum = prompt("Guess the number between 1 und 10 inclusive");
+
+        if (gnum == num)
+
+            console.log("Matched");
+
+            else 
+
+            console.log("Not matched, the number was" + "  "+  gnum);
+
+    
+
+
+    // Exercise 9:  Calculate days left until next Chrismas ( Noel e kalan günleri hesapla)
+
+
+    today=new Date();
+
+    var cmas = new Date(today.getFullYear(),11,25);
+
+    if (today.getMonth() == 11 && today.getDate()>25)
+
+    { 
+        cmas.setFullYear(cmas.getFullYear() + 1) ;
+
+    }
+
+    var one_day = 1000*60*60*24;  // 24 Stunden, 60 Minuten, 60 Sekunden, ve 1 saniye 1000 milisaniye oldugundan burada 1 günü milisaniye olarak hesaplamis.
+
+                                  // Cünkü jawascript de tarih 1970 yilindan itibaren mili saniye olarak hesaplanir.
+
+    console.log(Math.ceil((cmas.getTime()-today.getTime()) / (one_day) ) + "  " + "days left until Christmas" );
+
+
+
+    // ***** Math.ceil yöntemi, belirli bir sayidan büyük ya da o sayiya esit en  kücük tam sayiyi bulmak icin kullanilir.
+
+    // ***** GetTime Yöntemi, evrensel saate göre belirtilen tarihin zamanina karsilik gelen sayisal degeri almak icin
+
+    // ***** Get Date ayin gününü almak icin.  Bu deger 1 ile 31 arasinda döndürülür.
+
+    // ***** GetMonth, ayi sifir tabanli bir deger olarak almak icin kullanilir. Bu deger 0 ile 11 arasindadir. 0 Ocak, 1 Subat, 2 Mart und so weiter.
+
+    // ***** GetFullYear, belirtilen tarihin yilini almak icin kullanilir. Mutlak bir sayidir. 1000 ile 9999 arasindadöndürülür. 1985 gibi
+
+
+
+
+
+
+
+
+
+    // Exercise 10 To calculate multiplication und division of two numbers ( Iki sayinin carpilmasi ve bölünmesi)
+
+    // ***********  GetElementById(id).value,  bir metin alaninin deger ozelligini ayarlar ve döndürür. 
+
+    // ***********  InnerHtml,  Bir ögenin Html özelligini ayarlar ve döndürür.
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+            
+            
+    
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+  
+    
 
 
 
