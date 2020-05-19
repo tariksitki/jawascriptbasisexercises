7a1eeaca5f3f49be70465e764527003dad6e6659
tariksitki <tariksitki0119@gmail.com>
2020-05-19 23:59:24 +0200

    jawa 25

--- main.js
+++ main.js
@@ -852,3 +852,31 @@ console.log(diff_num(22));
             
             
             
+
+
+
+
+
+
+            // Exercise 25
+
+            function test37(x) {
+
+                if (x % 3 == 0 || x % 7 == 0) {
+
+                    return true ;
+                }
+                else {
+                    return false ;
+                }
+            }
+
+            console.log(test37(12));
+
+            console.log(test37(14));
+
+            console.log(test37(10));
+            
+            
+            
+            
