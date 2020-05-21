7708d85d61f0e1edd9c58cd7f704976f807a0c71
tariksitki <tariksitki0119@gmail.com>
2020-05-21 23:57:24 +0200

    jawascript 31

--- main.js
+++ main.js
@@ -1035,5 +1035,34 @@ console.log(check_script("CoffeeScript"));
 
 
 
-        
+
+
+
+
+
+
+            // Exercise 31 
+
+
+
+            function max_of_three(x, y, z) 
+ {
+  max_val = 0;
+  if (x > y)
+  {
+    max_val = x;
+  } else
+  {
+    max_val = y;
+  }
+  if (z > max_val) 
+  {
+    max_val = z;
+  }
+  return max_val;
+}
+
+console.log(max_of_three(1,0,1));
+console.log(max_of_three(0,-10,-20));
+console.log(max_of_three(1000,510,440));
             
