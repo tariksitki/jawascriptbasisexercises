2b7343cf1dbf9033f5a6a38078e07d0e5fc532df
tariksitki <tariksitki0119@gmail.com>
2020-05-28 23:54:34 +0200

    JavaScript 41

--- main.js
+++ main.js
@@ -1584,6 +1584,64 @@ console.log(max_of_three(1000,510,440));
 
 
 
+
+
+
+              // Exercise 41        
+
+
+
+
+
+              function three_numbers(x, y, z) {
+
+                if ( x == y && y == z) {
+
+                    return 30 ;
+                    
+                }
+
+
+                if ( x == y || y == z || x == z) {
+
+                    return 40 ;
+                    
+                }
+
+                    return 20;
+                  
+              }
+
+
+
+              console.log(three_numbers(8,8,8));
+
+              console.log(three_numbers(8,8,18));
+        
+              console.log(three_numbers(8,7,18));
+
+              
+              
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                   
                     
                     
