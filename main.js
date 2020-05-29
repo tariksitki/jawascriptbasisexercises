211ce9f536d80a1c979e99472bd18ef74047b81a
tariksitki <tariksitki0119@gmail.com>
2020-05-30 00:00:27 +0200

    47

--- main.js
+++ main.js
@@ -1791,7 +1791,22 @@ console.log(max_of_three(1000,510,440));
                         // Exercise 47 
 
 
-                        
+
+
+
+                        function test_digit(x, y, n) 
+  {    
+    if (n < 40 || n > 10000)
+      return false;
+    else
+      if (n >= x && n <= y)
+        return true;
+      else
+        return false;
+  }
+console.log(test_digit(40, 4000, 45));  
+console.log(test_digit(80, 320, 79));  
+console.log(test_digit(89, 4000, 30));
                             
                             
                             
