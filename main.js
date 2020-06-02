a60c7834d58f428e438c96dea77ffc509b6f52ef
tariksitki <tariksitki0119@gmail.com>
2020-06-02 22:27:11 +0200

    JavaScript 48 49

--- main.js
+++ main.js
@@ -1828,6 +1828,78 @@ console.log(max_of_three(1000,510,440));
                                     console.log(test_digit(89,4000,30));
 
 
+
+
+
+
+
+
+
+
+
+
+
+                                    // Exercise 48 
+
+
+
+
+
+                                    function string_reverse(str) 
+                                    {
+                                        return str.split("").reverse().join("");
+                                    }
+                                    
+                                    console.log(string_reverse("w3resource"));
+                                    console.log(string_reverse("www"));
+                                    console.log(string_reverse("JavaScript"));
+
+
+
+
+
+
+
+                                    //  Exercise 49
+
+
+
+
+
+
+
+                                    function string_reverse(str) 
+                                    function LetterChanges(text) {
+                                    //https://goo.gl/R8gn7u
+                                    var s = text.split('');
+                                    for (var i = 0; i < s.length; i++) {
+                                    // Caesar cipher
+                                    switch(s[i]) {
+                                    case ' ':
+                                    break;
+                                    case 'z':
+                                    s[i] = 'a';
+                                    break;
+                                    case 'Z': 
+                                    s[i] = 'A';
+                                    break;
+                                    default:
+                                    s[i] = String.fromCharCode(1 + s[i].charCodeAt(0));
+                                    }
+                                    
+                                    // Upper-case vowels
+                                    switch(s[i]) {
+                                    case 'a': case 'e': case 'i': case 'o': case 'u':
+                                    s[i] = s[i].toUpperCase();
+                                    }
+                                    }
+                                    return s.join('');
+                                    }
+                                    console.log(LetterChanges("PYTHON"));
+                                    console.log(LetterChanges("W3R"));
+                                    console.log(LetterChanges("php"));
+
+
                                     
                                     
                                     
